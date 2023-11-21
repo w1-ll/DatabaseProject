7d103e99eae36b3c68842bb942cec9a29fa8ed05
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2023-11-21 12:08:57 -0500

    Update tree.java

--- database/src/tree.java
+++ database/src/tree.java
@@ -1,27 +1,27 @@
 public class tree 
 {
-		protected String tree_distance;
-	 	protected String trunk_size;
-	    protected String tree_height;
-	    protected String tree_location;
+		protected int tree_distance;
+	 	protected int trunk_size;
+	    protected int tree_height;
+	    protected int tree_location;
 	    protected int tree_id;
 	    //constructors
 	    public tree() {
 	    }
 	 
-	    public tree(String tree_distance) 
+	    public tree(int tree_distance) 
 	    {
 	        this.tree_distance = tree_distance;
 	    }
 	    
-	    public tree(String tree_distance, String trunk_size, String tree_height, String tree_location) 
+	    public tree(int tree_distance, int trunk_size, int tree_height, int tree_location) 
 	    {
 	    	this(trunk_size,tree_height, tree_location);
 	    	this.tree_distance = tree_distance;
 	    }
 	 
 	
-	    public tree(String trunk_size, String tree_height, String tree_location) 
+	    public tree(int trunk_size, int tree_height, int tree_location) 
 	    {
 	    	this.trunk_size = trunk_size;
 	    	this.tree_height = tree_height;
@@ -29,36 +29,35 @@ public class tree
 	    }
 	    
 	   //getter and setter methods
-	    public String getTree_distance() {
+	    public int getTree_distance() {
 	        return tree_distance;
 	    }
-	    public void setTree_distance(String tree_distance) {
+	    public void setTree_distance(int tree_distance) {
 	        this.tree_distance = tree_distance;
 	    }
 	    
-	    public String getTrunk_size() {
+	    public int getTrunk_size() {
 	        return trunk_size;
 	    }
-	    public void setTrunk_size(String trunk_size) {
+	    public void setTrunk_size(int trunk_size) {
 	        this.trunk_size = trunk_size;
 	    }
 	    
-	    public String getTree_height() {
+	    public int getTree_height() {
 	        return tree_height;
 	    }
-	    public void setTree_height(String tree_height) {
+	    public void setTree_height(int tree_height) {
 	        this.tree_height = tree_height;
 	    }
 	        
 	    	    
-	    public String getTree_location() {
+	    public int getTree_location() {
 	        return tree_location;
 	    }
-	    public void setTree_location(String tree_location) {
+	    public void setTree_location(int tree_location) {
 	        this.tree_location = tree_location;
 	    }
 	    public int getTree_id() {
 	        return tree_id;
 	    }
 	}
-
