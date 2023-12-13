9907b5863b896faab851cda4261f52d7df28f7c5
rishithab48 <100251123+rishithab48@users.noreply.github.com>
2023-12-13 12:45:18 -0500

    Update quote.java

--- database/src/quote.java
+++ database/src/quote.java
@@ -8,14 +8,19 @@ public class quote
 	    protected int price;
 	    protected String email;
 	    protected String user_note;
-	    protected int tree_id;
+	    protected int unique_tree_id;
+	    protected boolean negotiationStatus;
 	    //constructors
 	    public quote() {
 	    }
-	 
+	    public void initializeNegotiationStatus() {
+	    	negotiationStatus = false;
+	    }
 	    public quote(String quoteID) 
 	    {
 	        this.quoteID = quoteID;
+	        initializeNegotiationStatus();
+	        
 	    }
 	    
 	    public quote(String quoteID, String status1, String status2, String negotiation_note,String work_period, int price,String email) 
@@ -29,13 +34,15 @@ public class quote
 	    {
 	    	this(quoteID,status1, status2,negotiation_note,work_period,price,email);
 	    	this.user_note = user_note;
+	    	
 	    }
 	    
-	    public quote(String quoteID, String status1, String status2, String negotiation_note,String work_period, int price,String email, String user_note,int tree_id) 
+	    public quote(String quoteID, String status1, String status2, String negotiation_note,String work_period, int price,String email, String user_note,int unique_tree_id) 
 	    {
 	    	this(quoteID,status1, status2,negotiation_note,work_period,price,email,user_note);
-	    	this.tree_id = tree_id;
-	    	System.out.println(this.tree_id);
+	    	this.unique_tree_id = unique_tree_id;
+	    	//System.out.println(this.tree_id);
+	    	
 	    }
 	
 	    public quote(String status1, String status2, String negotiation_note,String work_period, int price, String email) 
@@ -46,6 +53,7 @@ public class quote
 	    	this.negotiation_note = negotiation_note;
 	    	this.work_period = work_period;
 	    	this.price = price;
+	    	initializeNegotiationStatus();
 	    }
 	    
 	   //getter and setter methods
@@ -103,8 +111,8 @@ public class quote
 	    public void setUser_note(String note) {
 	    	this.user_note = note;
 	    }
-	    public int getTree_id() {
-	    	return tree_id;
+	    public int getUnique_tree_id() {
+	    	return unique_tree_id;
 	    }
 	    
 	}
